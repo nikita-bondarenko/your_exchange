d78e6bbb307225d63436a692782846496d9a5aea
Nikita Bondarenko <naradadasosmi@gmail.com>
2025-10-25 21:02:12 +0300

    fix page data theme setup

--- app/src/d__features/themeSwitcher/model/ThemeInitialiser.tsx
+++ app/src/d__features/themeSwitcher/model/ThemeInitialiser.tsx
@@ -1,11 +1,15 @@
 import { PROJECT_DATA } from "@/shared/config";
 import { memo, useEffect } from "react";
 import { switchTheme } from "../lib";
+import { setPageData, useAppDispatch } from "@/shared/model/store";
 
 export const ThemeInitialiser = () => {
-    // console.log('ThemeInitialiser')
-    useEffect(()=> {
-    switchTheme(PROJECT_DATA.theme)
-    },[])
-    return <></>
-}
+  // console.log('ThemeInitialiser')
+  const dispatch = useAppDispatch();
+
+  useEffect(() => {
+    switchTheme(PROJECT_DATA.theme);
+    dispatch(setPageData(PROJECT_DATA.page));
+  }, []);
+  return <></>;
+};
