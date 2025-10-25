600b460b192b3b3522f22ebd7669822e13fb23c7
Nikita Bondarenko <naradadasosmi@gmail.com>
2025-10-25 20:52:02 +0300

    update next.condif.ts

--- app/next.config.ts
+++ app/next.config.ts
@@ -1,10 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-
   reactStrictMode: false,
   productionBrowserSourceMaps: true,
-
+  dynamic: "force-dynamic",
 };
 
 export default nextConfig;
