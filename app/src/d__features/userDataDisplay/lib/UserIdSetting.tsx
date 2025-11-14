eaa0168906dd804cf557a7384f950266b8ed7e20
Nikita Bondarenko <naradadasosmi@gmail.com>
2025-11-15 02:08:25 +0300

    fix init loading bug

--- app/src/d__features/userDataDisplay/lib/UserIdSetting.tsx
+++ app/src/d__features/userDataDisplay/lib/UserIdSetting.tsx
@@ -1,56 +1,48 @@
 "use client";
-import { startTransition, useActionState, useEffect } from "react";
 import { setIsAppReady, useAppDispatch } from "@/shared/model/store";
 import { TEST_USER_ID } from "@/shared/config";
 import { setUserData, setUserId } from "@/d__features/userDataDisplay/model";
 import { getUserDataAction } from "../api/actions/getUserDataAction";
+import Script from "next/script";
 
 export function UserIdSetting() {
   const dispatch = useAppDispatch();
 
-  useEffect(() => {
-    const script = document.createElement("script");
-    script.src = "https://telegram.org/js/telegram-web-app.js";
-    script.async = true;
-    script.onload = () => {
-      if (window.Telegram?.WebApp) {
-        window.Telegram.WebApp.ready();
-        window.Telegram.WebApp.expand();
-        dispatch(setIsAppReady(true));
+  const handleScriptLoad = () => {
+    if (window.Telegram?.WebApp) {
+      window.Telegram.WebApp.ready();
+      window.Telegram.WebApp.expand();
+      dispatch(setIsAppReady(true));
 
-        let userId: number | null = null;
+      let userId: number | null = null;
 
-        if (process.env.NODE_ENV === "development") {
-          userId = TEST_USER_ID;
+      if (process.env.NODE_ENV === "development") {
+        userId = TEST_USER_ID;
+      } else {
+        if (window.Telegram?.WebApp?.initDataUnsafe?.user) {
+          userId = window.Telegram.WebApp.initDataUnsafe.user.id;
         } else {
-          if (window.Telegram?.WebApp?.initDataUnsafe?.user) {
-            userId = window.Telegram.WebApp.initDataUnsafe.user.id;
-          } else {
-            console.error("user Id not found");
-          }
-        }
-        console.log(userId);
-        if (userId) {
-          dispatch(setUserId(userId));
-          setTimeout(() => {
-            startTransition(() => {
-              getUserDataAction({ userId })
-                .then((result) => {
-                  console.log(result);
-                  if (result) dispatch(setUserData(result));
-                })
-                .catch(console.error);
-            });
-          }, 100);
+          console.error("user Id not found");
         }
       }
-    };
-    document.head.appendChild(script);
+      console.log(userId);
+      if (userId) {
+        dispatch(setUserId(userId));
+        getUserDataAction({ userId })
+          .then((result) => {
+            console.log(result)
+            if (result) dispatch(setUserData(result));
+          })
+          .catch(console.error);
+      }
+    }
+  };
 
-    return () => {
-      document.head.removeChild(script);
-    };
-  }, []);
-
-  return <></>;
+  return (
+    <Script
+      src="https://telegram.org/js/telegram-web-app.js"
+      strategy="afterInteractive"
+      onLoad={handleScriptLoad}
+    />
+  );
 }
