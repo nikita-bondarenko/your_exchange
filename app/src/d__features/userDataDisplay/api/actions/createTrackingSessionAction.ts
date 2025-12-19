6172cb5c0c10be32c9c9746c42e0f2783fb160a8
nikita-bondarenko <your-email@example.com>
2025-12-19 17:37:07 +0300

    update workflow and fix build problems

--- app/src/d__features/userDataDisplay/api/actions/createTrackingSessionAction.ts
+++ app/src/d__features/userDataDisplay/api/actions/createTrackingSessionAction.ts
@@ -3,22 +3,19 @@
 import { FetchApiProps, fetchApi } from "@/shared/lib/serverAction";
 import { CreateSessionArg, CreateSessionResponse } from "../../model";
 import { authenticateUser } from "@/d__features/userDataDisplay/lib/telegramAuth";
-import { PROJECT_NAME } from "@/shared/config";
 
 export async function createTrackingSessionAction(
   body: CreateSessionArg
 ): Promise<CreateSessionResponse> {
   const { initData, user_id } = body;
 
-  if (PROJECT_NAME === 'test') {
-    if (!initData) {
-      throw new Error("Telegram WebApp initData required");
-    }
+  if (!initData) {
+    throw new Error("Telegram WebApp initData required");
+  }
 
-    const authUserId = await authenticateUser(initData);
-    if (authUserId !== user_id) {
-      throw new Error("User ID mismatch");
-    }
+  const authUserId = await authenticateUser(initData);
+  if (authUserId !== user_id) {
+    throw new Error("User ID mismatch");
   }
 
   const props: FetchApiProps = {
