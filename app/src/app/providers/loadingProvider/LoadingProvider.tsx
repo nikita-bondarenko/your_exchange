2995c498da6113330e0024d4f045047c33081d6c
nikita-bondarenko <your-email@example.com>
2025-12-22 11:17:09 +0300

    update docker conf, add workflow

--- app/src/app/providers/loadingProvider/LoadingProvider.tsx
+++ app/src/app/providers/loadingProvider/LoadingProvider.tsx
@@ -1,13 +1,15 @@
 "use client";
 import clsx from "clsx";
-import { FC, useEffect, useState } from "react";
-import { useAppDispatch, useAppSelector } from "@/shared/model/store/hooks";
+import { FC } from "react";
 import { useGlobalLoading } from "./lib";
-import { setIsLoading } from "@/shared/model/store";
 
 export const LoadingProvider: FC<{ children: React.ReactNode }> = ({ children }) => {
     const { isLoading } = useGlobalLoading();
-  return <div className={clsx("shimmer-on-loading flex-grow",{"loading":isLoading})}>{children}</div>;
+  return (
+    <div className={clsx("shimmer-on-loading flex-grow relative",{"loading":isLoading})}>
+      {children}
+    </div>
+  );
 };
 
 LoadingProvider.displayName = "LoadingProvider";
--- app/src/shared/config/app.ts
+++ app/src/shared/config/app.ts
@@ -4,7 +4,8 @@ export const PROJECT_NAME = PROJECT_NAME_SERVER
 
 export const TEST_USER_ID = 464552386;
 
-export const TEST_USER_INIT_DATA = "user=%7B%22id%22%3A464552386%2C%22first_name%22%3A%22Nikita%22%2C%22last_name%22%3A%22Bondarenko%22%2C%22username%22%3A%22NikitaBondarenkoDev%22%2C%22language_code%22%3A%22ru%22%2C%22allows_write_to_pm%22%3Atrue%2C%22photo_url%22%3A%22https%3A%5C%2F%5C%2Ft.me%5C%2Fi%5C%2Fuserpic%5C%2F320%5C%2Fy6eMnOK-LDw1Jz3tcqc4rw09PwE2kCbH1O7PjFZt_94.svg%22%7D&chat_instance=3451593191344466144&chat_type=supergroup&auth_date=1766148163&signature=ljVCUsfk2mih0ZpMSS9Izt_rqwHLs47I2XAIfQv1DzBO6ednQRWfdeWJowiJFhHrCOUfvPlNsoRCAMtyW78wBQ&hash=11182220816d88fb7b44fef6d001ca1cd4eec832b3b60c982c4a2a990dd34ba8"
+export const TEST_USER_INIT_DATA = "user=%7B%22id%22%3A464552386%2C%22first_name%22%3A%22Nikita%22%2C%22last_name%22%3A%22Bondarenko%22%2C%22username%22%3A%22NikitaBondarenkoDev%22%2C%22language_code%22%3A%22ru%22%2C%22allows_write_to_pm%22%3Atrue%2C%22photo_url%22%3A%22https%3A%5C%2F%5C%2Ft.me%5C%2Fi%5C%2Fuserpic%5C%2F320%5C%2Fy6eMnOK-LDw1Jz3tcqc4rw09PwE2kCbH1O7PjFZt_94.svg%22%7D&chat_instance=-8670579429820676084&chat_type=private&auth_date=1766163778&signature=hstXOWozrAlcsBGAG5x6JKf79OoGBXInAZvKoS5lZWxG6nGUni3oGwu82D7ISLRD9JwaUVxPkrtkjyuWTGSOCQ&hash=d3370bcd240595b12db9275e61d21ca6ac1b0ba12dea4f60509fd36a73895ac9"
+
 
 export const PORTAL_TARGET_ID = "portal-target"
 
